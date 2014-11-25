7546d112d841d50a8f5eb65e634510712a1a1852
Ruairidh Finnigan <ruairidhfinnigan@gmail.com>
2014-11-25 13:40:35 +0000

    Implemented Fart Button that plays fart

--- FartShaker/gen/com/rfinnigan/fartshaker/R.java
+++ FartShaker/gen/com/rfinnigan/fartshaker/R.java
@@ -18,16 +18,17 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int container=0x7f080000;
-        public static final int edit_message=0x7f080001;
+        public static final int action_settings=0x7f090003;
+        public static final int container=0x7f090000;
+        public static final int edit_message=0x7f090001;
+        public static final int fartButton=0x7f090002;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
@@ -36,17 +37,21 @@ public final class R {
         public static final int fragment_main=0x7f030003;
     }
     public static final class menu {
-        public static final int display_message=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int display_message=0x7f080000;
+        public static final int main=0x7f080001;
+    }
+    public static final class raw {
+        public static final int fart=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050003;
-        public static final int app_name=0x7f050000;
-        public static final int button_send=0x7f050002;
-        public static final int edit_message=0x7f050001;
-        public static final int hello_world=0x7f050006;
-        public static final int title_activity_display_message=0x7f050005;
-        public static final int title_activity_main=0x7f050004;
+        public static final int action_settings=0x7f060004;
+        public static final int app_name=0x7f060000;
+        public static final int button_fart=0x7f060003;
+        public static final int button_send=0x7f060002;
+        public static final int edit_message=0x7f060001;
+        public static final int hello_world=0x7f060007;
+        public static final int title_activity_display_message=0x7f060006;
+        public static final int title_activity_main=0x7f060005;
     }
     public static final class style {
         /** 
@@ -70,10 +75,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- FartShaker/src/com/rfinnigan/fartshaker/MainActivity.java
+++ FartShaker/src/com/rfinnigan/fartshaker/MainActivity.java
@@ -3,27 +3,39 @@ package com.rfinnigan.fartshaker;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.MotionEvent;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
 	public final static String EXTRA_MESSAGE = "com.rfinnigan.fartshaker.MESSAGE";
+
+	private MediaPlayer mp;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+
+
+		mp = MediaPlayer.create(this, R.raw.fart);
+
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+			.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
+
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,12 +56,24 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	/**
+	 * called when fart button is pressed
+	 */
+	
+	public void playFart(View view) {
+		
+			
+			mp.start();
+		
+	}
+
 	/** 
 	 * Called when the user clicks the send button
 	 */
 	public void sendMessage(View view){
 		//do something in response to the button
-		
+
 		Intent intent = new Intent (this, DisplayMessageActivity.class); 
 		EditText editText = (EditText) findViewById(R.id.edit_message);
 		String message = editText.getText().toString();
@@ -73,5 +97,7 @@ public class MainActivity extends Activity {
 			return rootView;
 		}
 	}
+
+
 
 }
