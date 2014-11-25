b72a7758df33ba468dd78485eb6f651fe8f9847f
Ruairidh Finnigan <ruairidhfinnigan@gmail.com>
2014-11-25 12:37:47 +0000

    Remove some unnecessary code from DisplayMessageActivity

--- FartShaker/src/com/rfinnigan/fartshaker/DisplayMessageActivity.java
+++ FartShaker/src/com/rfinnigan/fartshaker/DisplayMessageActivity.java
@@ -44,21 +44,6 @@ public class DisplayMessageActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
 
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_display_message,
-					container, false);
-			return rootView;
-		}
-	}
 
 }
