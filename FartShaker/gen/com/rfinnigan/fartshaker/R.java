54cc894ad87fec18527eb6f86a276cbcd8f903f3
Ruairidh Finnigan <ruairidhfinnigan@gmail.com>
2014-11-26 14:28:25 +0000

    Change gitignore and commit button_icon

--- FartShaker/gen/com/rfinnigan/fartshaker/R.java
+++ FartShaker/gen/com/rfinnigan/fartshaker/R.java
@@ -22,7 +22,8 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button_icon=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f090003;
@@ -45,12 +46,13 @@ public final class R {
         public static final int fart2=0x7f040002;
     }
     public static final class string {
-        public static final int action_settings=0x7f060003;
+        public static final int action_settings=0x7f060004;
         public static final int app_name=0x7f060000;
         public static final int button_fart=0x7f060002;
         public static final int button_select=0x7f060001;
-        public static final int title_activity_display_message=0x7f060005;
-        public static final int title_activity_main=0x7f060004;
+        public static final int fart_button_desc=0x7f060003;
+        public static final int title_activity_display_message=0x7f060006;
+        public static final int title_activity_main=0x7f060005;
     }
     public static final class style {
         /** 
